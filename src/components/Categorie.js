7ccc90d44ae89a411b98f89ad2fe6bfa23437a2a
thierabsy <thierabsy@yahoo.fr>
2018-04-09 19:31:25 +0100

    Gh-pages

--- src/components/Categorie.js
+++ src/components/Categorie.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import queryString from 'query-string';
+// import queryString from 'query-string';
 import Ologo from './Ologo';
 import Header from './Header';
 import Slider from './Slider';
